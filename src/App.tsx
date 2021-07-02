f27590ebe2f6fee0d00c906f6cc38db3ca0919b8
Patrick Maciel <patrickmaciel.info@gmail.com>
2021-07-02 20:27:46 -0300

    Componente: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { GlobalStyle} from "./styles/global";
+import {Header} from "./components/Header";
 
 export function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
+      <Header />
       <h1>Hello World</h1>
-    </div>
+    </>
   );
 }
