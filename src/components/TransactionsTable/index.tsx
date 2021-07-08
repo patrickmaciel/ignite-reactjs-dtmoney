a1969046a63b34857a7d246dd6d78b9173fae6a4
Patrick Maciel <patrickmaciel.info@gmail.com>
2021-07-08 20:45:25 -0300

    Listando transações e seeds

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,21 @@
 import { Container } from './styles'
-import {useEffect} from "react";
+import {useEffect, useState} from "react"
 import {api} from "../../services/api";
 
+interface TransactionsProps {
+  id: number,
+  title: string,
+  type: string,
+  amount: number,
+  category: string,
+  createdAt: string
+}
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<TransactionsProps[]>([])
+
   useEffect(() => {
     api.get('transactions')
-      .then(response => console.log(response.data))
+      .then(response => setTransactions(response.data.transactions))
   })
 
   return (
@@ -21,36 +31,14 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-              <td>Desenvolvimento de Website</td>
-              <td className='deposit'>R$12.000,00</td>
-              <td>Desenvolvimento</td>
-              <td>20/02/2021</td>
-          </tr>
-          <tr>
-              <td>Energia</td>
-              <td className='withdraw'>-R$350,00</td>
-              <td>Casa</td>
-              <td>20/02/2021</td>
-          </tr>
-          <tr>
-              <td>Internet</td>
-              <td className='withdraw'>-R$263,00</td>
-              <td>Casa</td>
-              <td>20/02/2021</td>
-          </tr>
-          <tr>
-              <td>Desenvolvimento de Website</td>
-              <td className='deposit'>R$12.000,00</td>
-              <td>Desenvolvimento</td>
-              <td>20/02/2021</td>
-          </tr>
-          <tr>
-              <td>Desenvolvimento de Website</td>
-              <td>R$12.000,00</td>
-              <td>Desenvolvimento</td>
-              <td>20/02/2021</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
