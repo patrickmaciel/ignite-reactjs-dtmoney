cfb687551ad2b423a9eca24a87042940b7e7689c
Patrick Maciel <patrickmaciel.info@gmail.com>
2021-07-09 20:54:20 -0300

    Finalizando inserção

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -18,15 +18,21 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
   const [amount, setAmount] = useState(0)
   const [category, setCategory] = useState('')
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type
     })
+
+    setTitle('')
+    setType('deposit')
+    setAmount(0)
+    setCategory('')
+    onRequestClose()
   }
 
   return (
